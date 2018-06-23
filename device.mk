0e0eb14c0155f7eae63a4cf0437760209b64139f
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    ares: Build only system HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -89,8 +89,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0_system
 
 # Ims
 PRODUCT_BOOT_JARS += \
